8c3a4af2760f36bd2bc2329ad4a671387eafba04
hakanolgun <kaptanhakanolgun@gmail.com>
2021-10-06 17:16:52 +0300

    tekil foto sayfası update işlemi put metodunu simüle eden method-override modülü ile tamamlandı

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
 const path = require("path");
+const fs = require("fs");
 const Photo = require("./models/Photo");
 
 const app = express();
@@ -19,10 +22,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public")); // sayfaların public klasöründeki statik dosyalara ulaşması için bu middleware gerekli
 app.use(express.urlencoded({ extended: true })); // post işleminde form dataların okuyabilmek ve db'ye gönderebilmek için bu iki middleware gerekli
 app.use(express.json()); //post metodunda urlde gönderilen datayı json formatına çevirmek için kullanıyoruz
+app.use(fileUpload()); // dosya yükelemede gerekli fileupload middlewareini kullanmak için bu kodu yazıyoruz
+app.use(methodOverride("_method")); // override with POST having ?_method=DELETE
 
 // ROUTE
 app.get("/", async (req, res) => {
-  const photos = await Photo.find(); // tüm fotoları dbden çekip anasayfaya gönderdik
+  const photos = await Photo.find().sort("-dateCreated"); // tüm fotoları dbden çekip anasayfaya gönderdik
   res.render("index", {
     photos: photos,
   });
@@ -37,8 +42,49 @@ app.get("/add", (req, res) => {
 });
 
 app.post("/photos", async (req, res) => {
-  await Photo.create(req.body);
+  console.log(req.files.image);
+  // await Photo.create(req.body);
+  // res.redirect("/");
+
+  const uploadDir = "public/uploads";
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadedImage.name,
+    });
+  });
   res.redirect("/");
+});
+
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", {
+    photo,
+  });
+});
+
+app.get("/photos/edit/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render("edit", {
+    photo,
+  });
+});
+
+app.put("/photos/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 // LISTEN PORT
